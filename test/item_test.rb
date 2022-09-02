62deeee079b518d1cdd40cf56364cff92bc00340
Nikola Nikolov <nikolanenchevnikolov@gmail.com>
2022-09-02 12:21:09 +0300

    Added struct for item and its corresponding tests

---
+++ test/item_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+require 'item'
+
+class ItemTest < Minitest::Test
+  def setup
+    @item = Item.new('001',"Very Cheap Chair", 9.25)
+  end
+
+  def test_should_have_price
+    assert_equal 9.25, @item.price
+  end
+
+  def test_should_have_name
+    assert_equal 'Very Cheap Chair', @item.name
+  end
+
+  def test_should_have_product_code
+    assert_equal '001', @item.product_code
+  end
+end
